99a39d4b9fcbd44ad98f531537111651c1b2dea8
umarbashirr <umarbashir93@outlook.com>
2023-12-31 21:25:47 +0530

    Update ProjectSchema in project.model.ts

--- src/models/task-management/project.model.ts
+++ src/models/task-management/project.model.ts
@@ -1,19 +1,33 @@
 import { Schema, model } from "mongoose";
 
-const ProjectSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: { type: String },
-  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
-  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  owner: { type: Schema.Types.ObjectId, ref: "User" },
-  isDeleted: { type: Boolean, default: false },
-  deletedAt: { type: String },
-  deletedBy: { type: Schema.Types.ObjectId, ref: "User" },
-});
+export interface IProject {
+  name: string;
+  description: string;
+  tasks: string[];
+  members: string[];
+  owner: string;
+  isDeleted: boolean;
+  deletedAt: string;
+  deletedBy: string;
+}
 
-const Project = model("Project", ProjectSchema);
+const ProjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: { type: String },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    isDeleted: { type: Boolean, default: false },
+    deletedAt: { type: String },
+    deletedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Project = model<IProject>("Project", ProjectSchema);
 
 export default Project;
