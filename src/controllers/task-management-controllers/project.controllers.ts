63782666a198bb2e4cc7f0d66cade55d5dda3b59
umarbashirr <umarbashir93@outlook.com>
2023-12-31 21:38:44 +0530

    Add getAllProjects controller to fetch projects by owner

--- src/controllers/task-management-controllers/project.controllers.ts
+++ src/controllers/task-management-controllers/project.controllers.ts
@@ -1,7 +1,17 @@
 import { Response } from "express";
 import { asyncHandler } from "../../utils/asyncHandler";
+import Project from "../../models/task-management/project.model";
+import { ApiResponse } from "../../utils/ApiResponse";
 
-const getAllProjects = asyncHandler(async (req: any, res: Response) => {});
+const getAllProjects = asyncHandler(async (req: any, res: Response) => {
+  const userId = req?.user?._id;
+
+  const projects = await Project.find({ owner: userId });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Projects fetched successfully", projects));
+});
 
 const getProjectById = asyncHandler(async (req: any, res: Response) => {});
 
