98b909a16bc8a8cd3a4d2340d4a052a40e1d2011
umarbashirr <umarbashir93@outlook.com>
2023-12-31 21:55:56 +0530

    Refactor project controllers to include owner and isDeleted fields

--- src/controllers/task-management-controllers/project.controllers.ts
+++ src/controllers/task-management-controllers/project.controllers.ts
@@ -2,18 +2,32 @@ import { Response } from "express";
 import { asyncHandler } from "../../utils/asyncHandler";
 import Project from "../../models/task-management/project.model";
 import { ApiResponse } from "../../utils/ApiResponse";
+import { ApiError } from "../../utils/ApiError";
 
 const getAllProjects = asyncHandler(async (req: any, res: Response) => {
   const userId = req?.user?._id;
 
-  const projects = await Project.find({ owner: userId });
+  const projects = await Project.find({ owner: userId, isDeleted: false });
 
   res
     .status(200)
     .json(new ApiResponse(200, "Projects fetched successfully", projects));
 });
 
-const getProjectById = asyncHandler(async (req: any, res: Response) => {});
+const getProjectById = asyncHandler(async (req: any, res: Response) => {
+  const userId = req?.user?._id;
+  const projectId = req.params.id;
+
+  const project = await Project.findOne({
+    _id: projectId,
+    owner: userId,
+    isDeleted: false,
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Project fetched successfully", project));
+});
 
 const createProject = asyncHandler(async (req: any, res: Response) => {
   const userId = req?.user?._id;
@@ -32,9 +46,66 @@ const createProject = asyncHandler(async (req: any, res: Response) => {
     .json(new ApiResponse(201, "Project created successfully", newProject));
 });
 
-const updateProject = asyncHandler(async (req: any, res: Response) => {});
+const updateProject = asyncHandler(async (req: any, res: Response) => {
+  const userId = req?.user?._id;
+  const projectId = req.params.id;
 
-const deleteProject = asyncHandler(async (req: any, res: Response) => {});
+  const { name, description } = req.body;
+
+  const updatedProject = await Project.findOneAndUpdate(
+    {
+      _id: projectId,
+      owner: userId,
+      isDeleted: false,
+    },
+    {
+      name,
+      description,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!updatedProject) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Project updated successfully", updatedProject));
+});
+
+const deleteProject = asyncHandler(async (req: any, res: Response) => {
+  const userId = req?.user?._id;
+  const projectId = req.params.id;
+
+  const deletedProject = await Project.findOneAndUpdate(
+    {
+      _id: projectId,
+      owner: userId,
+      isDeleted: false,
+    },
+    {
+      isDeleted: true,
+      deletedAt: new Date().toISOString(),
+      deletedBy: userId,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!deletedProject) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Project deleted successfully", deletedProject));
+});
 
 export {
   getAllProjects,
