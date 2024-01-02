71764438c9ae6a86406496ce4671dca5d6a2fb22
umarbashirr <umarbashir93@outlook.com>
2024-01-02 16:46:22 +0530

    Remove deleted comment fields

--- src/models/task-management/comment.model.ts
+++ src/models/task-management/comment.model.ts
@@ -5,9 +5,6 @@ const CommentSchema = new Schema(
     description: { type: String, required: true },
     taskId: { type: Schema.Types.ObjectId, ref: "Task", required: true },
     commentedBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    isDeleted: { type: Boolean, default: false },
-    deletedAt: { type: String },
-    deletedBy: { type: Schema.Types.ObjectId, ref: "User" },
   },
   {
     timestamps: true,
